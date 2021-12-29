37fdcf8547fe5a7ce4f7be227873933a50133c94
P. Envall <petter.envall@gmail.com>
2021-12-29 11:26:50 +0100

    enable chooseing frontend framework (react/svelte)

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
   isDirEmpty,
   obtainDirectory,
 } from "./util.js";
-import { blue, bold, cyan, gray, yellow } from "kleur/colors";
+import { blue, bold, cyan, gray, yellow, red } from "kleur/colors";
 
 const argv = yargs(hideBin(process.argv))
   .usage(
@@ -18,6 +18,14 @@ const argv = yargs(hideBin(process.argv))
   .positional("dir", {
     describe: "directory to clone app to",
     type: "string",
+  })
+  .option("react", {
+    describe: "use React.js",
+    type: "boolean",
+  })
+  .option("svelte", {
+    describe: "use svelte",
+    type: "boolean",
   })
   .option("force", {
     alias: "f",
@@ -31,10 +39,25 @@ const argv = yargs(hideBin(process.argv))
     description: "Verbose output",
     default: false,
   })
+  .conflicts("react", "svelte")
   .parse();
+
 const [dir = "./"] = argv._;
-const { force: isForce } = argv;
-console.log({ dir, isForce });
+const { verbose, force: isForce, svelte, react = true } = argv;
+function decideFramework({ svelte, react }) {
+  switch (true) {
+    case svelte:
+      return "svelte";
+    case react:
+      return "react";
+  }
+  return null;
+}
+const framework = decideFramework({ svelte, react });
+if (!framework) {
+  console.error(red(`# Error: No framework specified.`));
+  process.exit(1);
+}
 
 const { version } = JSON.parse(
   fse.readFileSync(new URL("../package.json", import.meta.url), "utf-8")
@@ -42,7 +65,7 @@ const { version } = JSON.parse(
 
 const REPO = "git@github.com:npup/dask-app.git";
 
-export async function main(repo, targetName, verbose) {
+export async function main(repo, { dir: targetName, framework, verbose }) {
   console.log(`\n${bold("Welcome to DASK!")} ${gray(`(dask v${version})`)}`);
   console.log(
     `If you encounter a problem, visit ${cyan(
@@ -60,7 +83,7 @@ export async function main(repo, targetName, verbose) {
 
   // time to degit
   console.info(blue(`cloning to ${target}`));
-  clone({ repo, target, targetName, verbose });
+  clone({ repo: `${repo}#version/${framework}`, target, targetName, verbose });
 }
 
-main(REPO, dir, argv.verbose);
+main(REPO, { dir, framework, verbose });
